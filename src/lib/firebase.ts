73d99259bce66aceefc1313b40d313a36f07a7f6
Tejas Mishra <tejas.mishra.care@gmail.com>
2025-09-20 14:35:50 +0000

    File changes

---
+++ src/lib/firebase.ts
@@ -0,0 +1,16 @@
+// src/lib/firebase.ts
+import { initializeApp, getApp, getApps } from 'firebase/app';
+
+const firebaseConfig = {
+  projectId: "studio-9295250327-e0fca",
+  appId: "1:858476699204:web:e32b77743aaa18e56f2611",
+  apiKey: "AIzaSyCpoD_CMCccniOaBrirPyPcMGsfrqgn65Y",
+  authDomain: "studio-9295250327-e0fca.firebaseapp.com",
+  measurementId: "",
+  messagingSenderId: "858476699204"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+export { app };
