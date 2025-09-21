48b721f32455b505103c490ac7b93f1d6786cb4b
Tejas Mishra <tejas.mishra.care@gmail.com>
2025-09-21 05:21:42 +0000

    *You are an expert UI/UX designer and front-end developer. Your task is

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import './globals.css';
 import { cn } from '@/lib/utils';
 import { UserProfileProvider } from '@/hooks/use-user-profile.tsx';
 import { Toaster } from '@/components/ui/toaster';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export const metadata: Metadata = {
   title: 'CareerCompassAI',
@@ -15,20 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className={cn('font-body antialiased')}>
+    <html lang="en" className="dark" style={{ colorScheme: 'dark' }}>
+      <body className={cn('font-body antialiased', inter.variable)}>
         <UserProfileProvider>
           {children}
           <Toaster />
