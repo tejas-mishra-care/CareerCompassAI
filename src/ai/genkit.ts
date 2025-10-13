bf3c33300e3a5857e30a5978998d0353eea0c2ef
Tejas Mishra <tejas.mishra.care@gmail.com>
2025-10-13 07:58:27 +0000

    Build Error
    Module not found: Can't resolve '@genkit-ai/google-genai'

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,8 +1,8 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/google-genai';
+// import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
-  plugins: [googleAI({
+  plugins: [/*googleAI({
     model: 'gemini-1.5-flash-latest'
-  })],
+  })*/],
 });
